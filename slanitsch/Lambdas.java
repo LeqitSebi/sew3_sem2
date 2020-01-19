8684583ae576692fc9668db2fae5f9d3f1855ab1
Sebastian Slanitsch <7225@htl.rennweg.at>
2020-01-19 21:45:33 +0100

    erstellen der benötigten Dateien für Streams fertigstellen von Lambdas bis auf numerisch

--- slanitsch/Lambdas.java
+++ slanitsch/Lambdas.java
@@ -1,9 +1,10 @@
 package slanitsch;
 
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.function.UnaryOperator;
 
 public class Lambdas {
 
@@ -13,8 +14,16 @@ public class Lambdas {
      */
     public static void main(String[] args) {
         Collection<String> test = new LinkedList<>(Arrays.asList("Test", "TEST", "test", "Test2", "Passt"));
+        List<Double> zahlen = new LinkedList<>(Arrays.asList(2.2, 4.4, 0.3, 1.0, 6.0));
         print(test);
         System.out.println(hauptworte(test));
+        System.out.println(mult(zahlen, 2.0));
+        System.out.println(func(zahlen, new UnaryOperator<Double>() {
+            @Override
+            public Double apply(Double aDouble) {
+                return aDouble*5;
+            }
+        }));
     }
 
     /**
@@ -31,10 +40,21 @@ public class Lambdas {
      * @return Liste aus Hauptwörtern
      */
     private static Collection<String> hauptworte(Collection<String> worte){
-        worte.removeIf(wort ->
-                (wort.charAt(0)<='A')
-                        || (wort.charAt(0)>='Z')
-                        || (!wort.matches("[A-Za-z]+")));
+        worte.removeIf(wort -> (!wort.matches("[A-Z][A-Za-z]+")));
         return worte;
+    }
+
+    private static List<Double> mult(List<Double> zahlen, double faktor){
+        zahlen.replaceAll(zahl -> zahl*faktor);
+        return zahlen;
+    }
+
+    private static List<Double> func(List<Double> zahlen, UnaryOperator<Double> op){
+        zahlen.replaceAll(zahl -> op.apply(zahl));
+        return zahlen;
+    }
+
+    private static List<Double> numerisch(String ... elements){
+        return null;
     }
 }
