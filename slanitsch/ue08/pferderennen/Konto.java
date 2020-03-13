fdcc1d3e71f3fc84a50262da7592cb6a20cba044
slanitsch <7225@htl.rennweg.at>
2020-03-13 23:55:53 +0100

    first version of the horst race bet system not functional number of horses not choosable

---
+++ slanitsch/ue08/pferderennen/Konto.java
@@ -0,0 +1,64 @@
+package slanitsch.ue08.pferderennen;
+
+public class Konto {
+    int kontostand;
+
+    /**
+     * Empty constructor.
+     * initialises account with balance 0€
+     */
+    public Konto(){
+        kontostand = 0;
+    }
+
+    /**
+     * Constructor given all variables.
+     * @param kontostand balance that the account should be initialised with
+     */
+    public Konto(int kontostand){
+        this.kontostand = kontostand;
+    }
+
+    /**
+     * gets the current balance of an account
+     * @return current balance as an Integer
+     */
+    public int getKontostand() {
+        return this.kontostand;
+    }
+
+    /**
+     * sets the balance of an account
+     * @param kontostand balance to set to as an Integer
+     */
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+
+    /**
+     * adds a specific balance to an account
+     * @param betrag amount to add
+     */
+    void add(int betrag){
+        this.kontostand += betrag;
+    }
+
+    /**
+     * Converts an account to a printable useful string.
+     * @return Kontostand + current balance with decimals and € as a String
+     */
+    @Override
+    public String toString(){
+        String kontostandstr = Integer.toString(kontostand);
+        StringBuilder output = new StringBuilder();
+        int helper = (kontostandstr.length()%3);
+        if (helper!=0) {
+            output.append(kontostandstr, 0, helper).append(".");
+        }
+        String tripples = kontostandstr.substring(helper);
+        for (int i = 0; i < tripples.length(); i+=3) {
+            output.append(tripples, i, i + 3).append(".");
+        }
+        return output.substring(0, output.length()-1) + "€";
+    }
+}
---
+++ slanitsch/ue08/pferderennen/pferderennen.java
@@ -0,0 +1,177 @@
+package slanitsch.ue08.pferderennen;
+
+
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class pferderennen extends javax.swing.JFrame {
+    public static void main(String args[]){
+        Konto useraccount = new Konto(5000);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        JFrame frame = new JFrame("Pferderennen");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize((int) width, (int) height);
+        frame.setLocation(0, 0);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Amount (in €)");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JLabel label2 = new JLabel("Enter Horse");
+        JTextField tf2 = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        JButton startbutt = new JButton("Start Race");
+        panel.add(label2);
+        panel.add(tf2);
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+        panel.add(startbutt);
+
+        // Text Area at the Center
+        JTextPane ta = new JTextPane();
+        ta.setEditable(false);
+        ta.setPreferredSize(new Dimension((int) (width/6), (int) height));
+        ta.setText("Event Log:");
+
+        Font font =  new Font("Verdana", Font.BOLD, 16);
+        JLabel balance = new JLabel("Balance: " + useraccount);
+        balance.setFont(font);
+        StyledDocument doc = ta.getStyledDocument();
+
+        Style style = ta.addStyle("Test", null);
+        Style stylegreen = ta.addStyle("Test", null);
+        StyleConstants.setForeground(style, Color.blue);
+        StyleConstants.setForeground(stylegreen, Color.green);
+
+        JPanel horses = new JPanel(new GridLayout(10, 0));
+        JProgressBar horse1 = new JProgressBar(0, 100);
+        JProgressBar horse2 = new JProgressBar(0, 100);
+        JProgressBar horse3 = new JProgressBar(0, 100);
+        horse1.setSize(new Dimension((int) width, 30));
+        horse2.setSize(new Dimension((int) width, 30));
+        horse3.setSize(new Dimension((int) width, 30));
+        horse1.setValue(0);
+        horse2.setValue(0);
+        horse3.setValue(0);
+
+        horses.add(horse1);
+        horses.add(horse2);
+        horses.add(horse3);
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(horses);
+        frame.getContentPane().add(BorderLayout.NORTH, balance);
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.EAST, ta);
+        frame.setVisible(true);
+
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try { doc.insertString(doc.getLength(), "\nPlaced " + tf.getText() + "€ on Horse " + tf2.getText(),style); }
+                catch (BadLocationException error){}
+            }
+        });
+
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tf.setText("");
+                tf2.setText("");
+            }
+        });
+
+        AtomicLong startTime = new AtomicLong();
+        AtomicLong timeH1AT = new AtomicLong();
+        AtomicLong timeH2AT = new AtomicLong();
+        AtomicLong timeH3AT = new AtomicLong();
+
+        AtomicBoolean WinnerDeclared = new AtomicBoolean(false);
+
+        startbutt.addActionListener(ev -> {
+            Thread t = new Thread(() -> {
+                startTime.set(System.currentTimeMillis());
+                for (double i = 0; i <= 100; i++) {
+                    double value = i;
+                    horse1.setValue((int) i);
+                    try {
+                        double rnd = Math.random()*100;
+                        Thread.sleep((long) rnd);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                timeH1AT.set(System.currentTimeMillis());
+                if (!WinnerDeclared.get()){
+                    WinnerDeclared.set(true);
+                    try {
+                        doc.insertString(doc.getLength(), "\nHorse 1 won with a time of " + (timeH1AT.get() - startTime.get())/1000.00 + "seconds", stylegreen);
+                    } catch (BadLocationException error) {
+                    }
+                }
+            });
+            t.start();
+        });
+
+        startbutt.addActionListener(ev -> {
+            Thread t = new Thread(() -> {
+                for (double i = 0; i <= 100; i++) {
+                    double value = i;
+                    horse2.setValue((int) i);
+                    try {
+                        double rnd = Math.random()*100;
+                        Thread.sleep((long) rnd);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                timeH2AT.set(System.currentTimeMillis());
+                if (!WinnerDeclared.get()){
+                    WinnerDeclared.set(true);
+                    try {
+                        doc.insertString(doc.getLength(), "\nHorse 2 won with a time of " + (timeH2AT.get() - startTime.get())/1000.00 + "seconds", stylegreen);
+                    } catch (BadLocationException error) {
+                    }
+                }
+            });
+            t.start();
+        });
+
+        startbutt.addActionListener(ev -> {
+            Thread t = new Thread(() -> {
+                for (double i = 0; i <= 100; i++) {
+                    double value = i;
+                    horse3.setValue((int) i);
+                    try {
+                        double rnd = Math.random()*100;
+                        Thread.sleep((long) rnd);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                timeH3AT.set(System.currentTimeMillis());
+                if (!WinnerDeclared.get()){
+                    WinnerDeclared.set(true);
+                    try {
+                        doc.insertString(doc.getLength(), "\nHorse 3 won with a time of " + (timeH3AT.get() - startTime.get())/1000.00 + "seconds", stylegreen);
+                    } catch (BadLocationException error) {
+                    }
+                }
+            });
+            t.start();
+        });
+    }
+}
