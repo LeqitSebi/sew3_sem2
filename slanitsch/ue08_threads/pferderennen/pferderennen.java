582d43f5826e5ac06e69d898241da1e26e87d2e8
slanitsch <7225@htl.rennweg.at>
2020-03-31 16:08:30 +0200

    completely reworking pferderennen.java

--- slanitsch/ue08_threads/pferderennen/pferderennen.java
+++ slanitsch/ue08_threads/pferderennen/pferderennen.java
@@ -1,188 +1,201 @@
 package slanitsch.ue08_threads.pferderennen;
 
-import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Style;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
-import java.awt.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class pferderennen extends javax.swing.JFrame {
-    public static void main(String[] args){
-        Konto useraccount = new Konto(5000);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        double width = screenSize.getWidth();
-        double height = screenSize.getHeight();
-        JFrame frame = new JFrame("Pferderennen");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize((int) width, (int) height);
-        frame.setLocation(0, 0);
-        frame.setVisible(true);
+import java.util.LinkedList;
+import java.util.List;
 
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Amount (in €)");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JLabel label2 = new JLabel("Enter Horse(1-3)");
-        JTextField tf2 = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        JButton startbutt = new JButton("Start Race");
-        panel.add(label2);
-        panel.add(tf2);
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
-        panel.add(startbutt);
 
-        // Text Area at the Center
-        JTextPane ta = new JTextPane();
-        ta.setEditable(false);
-        ta.setPreferredSize(new Dimension((int) (width/6), (int) height));
-        ta.setText("Event Log:");
-
-        Font font =  new Font("Verdana", Font.BOLD, 16);
-        //TODO: JLabel doesn't update
-        final JLabel[] balance = {new JLabel("Balance: " + useraccount)};
-        balance[0].setFont(font);
-        StyledDocument doc = ta.getStyledDocument();
-
-        Style style = ta.addStyle("Test", null);
-        Style stylegreen = ta.addStyle("Test", null);
-        StyleConstants.setForeground(style, Color.blue);
-        StyleConstants.setForeground(stylegreen, Color.green);
-
-        JPanel horses = new JPanel(new GridLayout(10, 0));
-        JProgressBar horse1 = new JProgressBar(0, 100);
-        JProgressBar horse2 = new JProgressBar(0, 100);
-        JProgressBar horse3 = new JProgressBar(0, 100);
-        horse1.setSize(new Dimension((int) width, 30));
-        horse2.setSize(new Dimension((int) width, 30));
-        horse3.setSize(new Dimension((int) width, 30));
-        horse1.setValue(0);
-        horse2.setValue(0);
-        horse3.setValue(0);
-
-        horses.add(horse1);
-        horses.add(horse2);
-        horses.add(horse3);
-
-        //Adding Components to the frame.
-        frame.getContentPane().add(horses);
-        frame.getContentPane().add(BorderLayout.NORTH, balance[0]);
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.EAST, ta);
-        frame.setVisible(true);
-
-        final int[] bets = new int[]{0, 0, 0};
-
-        send.addActionListener(e -> {
-            switch (tf2.getText()) {
-                case "1":
-                    bets[0] = Integer.parseInt(tf.getText());
-                    break;
-                case "2":
-                    bets[1] = Integer.parseInt(tf.getText());
-                    break;
-                case "3":
-                    bets[2] = Integer.parseInt(tf.getText());
-                    break;
-            }
-            int sum = bets[0] + bets[1] + bets[2];
-            useraccount.add(-sum);
-            balance[0].setText("Balance: " + useraccount);
-            try { doc.insertString(doc.getLength(), "\nPlaced " + tf.getText() + "€ on Horse " + tf2.getText(),style); }
-            catch (BadLocationException ignored){}
-        });
-
-        reset.addActionListener(e -> {
-            tf.setText("");
-            tf2.setText("");
-        });
-
-        AtomicLong startTime = new AtomicLong();
-        AtomicLong timeH1AT = new AtomicLong();
-        AtomicLong timeH2AT = new AtomicLong();
-        AtomicLong timeH3AT = new AtomicLong();
-
-        AtomicBoolean WinnerDeclared = new AtomicBoolean(false);
-
-        startbutt.addActionListener(ev -> {
-            Thread t = new Thread(() -> {
-                startTime.set(System.currentTimeMillis());
-                for (double i = 0; i <= 100; i++) {
-                    horse1.setValue((int) i);
-                    try {
-                        double rnd = Math.random()*100;
-                        Thread.sleep((long) rnd);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                timeH1AT.set(System.currentTimeMillis());
-                useraccount.add(bets[0]*2);
-                balance[0].setText("Balance: " + useraccount);
-                if (!WinnerDeclared.get()){
-                    WinnerDeclared.set(true);
-                    try {
-                        doc.insertString(doc.getLength(), "\nHorse 1 won with a time of " + (timeH1AT.get() - startTime.get())/1000.00 + "seconds", stylegreen);
-                    } catch (BadLocationException ignored) {
-                    }
-                }
-            });
-            t.start();
-        });
-
-        startbutt.addActionListener(ev -> {
-            Thread t = new Thread(() -> {
-                for (double i = 0; i <= 100; i++) {
-                    horse2.setValue((int) i);
-                    try {
-                        double rnd = Math.random()*100;
-                        Thread.sleep((long) rnd);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                timeH2AT.set(System.currentTimeMillis());
-                useraccount.add(bets[1]*2);
-                balance[0].setText("Balance: " + useraccount);
-                if (!WinnerDeclared.get()){
-                    WinnerDeclared.set(true);
-                    try {
-                        doc.insertString(doc.getLength(), "\nHorse 2 won with a time of " + (timeH2AT.get() - startTime.get())/1000.00 + "seconds", stylegreen);
-                    } catch (BadLocationException ignored) {
-                    }
-                }
-            });
-            t.start();
-        });
-
-        startbutt.addActionListener(ev -> {
-            Thread t = new Thread(() -> {
-                for (double i = 0; i <= 100; i++) {
-                    horse3.setValue((int) i);
-                    try {
-                        double rnd = Math.random()*100;
-                        Thread.sleep((long) rnd);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                timeH3AT.set(System.currentTimeMillis());
-                useraccount.add(bets[2]*2);
-                balance[0].setText("Balance: " + useraccount);
-                if (!WinnerDeclared.get()){
-                    WinnerDeclared.set(true);
-                    try {
-                        doc.insertString(doc.getLength(), "\nHorse 3 won with a time of " + (timeH3AT.get() - startTime.get())/1000.00 + "seconds", stylegreen);
-                    } catch (BadLocationException ignored) {
-                    }
-                }
-            });
-            t.start();
-        });
+public class pferderennen extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
+
+    int count = 2;
+
+    public void addProgressbar(VBox vBox) {
+        ProgressBar p = new ProgressBar();
+        p.setMaxWidth(Double.MAX_VALUE);
+        p.progressProperty().set(0);
+        vBox.getChildren().addAll(p);
+        count += 1;
+        String text = textArea.getText();
+        textArea.setText(text + "\n Es gibt " + count + " Pferde");
+    }
+
+    public void subProgressbar() {
+        progressbar.getChildren().remove(1);
+        count -= 1;
+        String text = textArea.getText();
+        if (count != 1) {
+            textArea.setText(text + "\nEs gibt " + count + " Pferde");
+        } else {
+            textArea.setText(text + "\nEs gibt 1 Pferd");
+        }
+    }
+
+    public void runThreads() {
+        String text = textArea.getText();
+        textArea.setText(text + "\nErgebnis:");
+        List<Pferd> pferde = new LinkedList<>();
+        for (int i = 0; i < progressbar.getChildren().size(); i++) {
+            pferde.add(new Pferd(i + 1, progressbar.getChildren().get(i)));
+        }
+
+        for (int i = 0; i < pferde.size(); i++) {
+            pferde.get(i).start();
+        }
+    }
+
+    VBox progressbar = new VBox();
+
+    public TextArea textArea = new TextArea();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Pferderennen");
+        stage.setMinWidth(600);
+        stage.setMinHeight(600);
+
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane, 800, 550);
+        stage.setScene(scene);
+
+        Label label = new Label("Winchester Pferderennen");
+
+        VBox buttons = new VBox();
+        Button plus = new Button("+");
+        plus.setPadding(new Insets(10));
+        Button minus = new Button("-");
+        minus.setPadding(new Insets(10, 12, 10, 12));
+        buttons.getChildren().addAll(plus, minus);
+
+        ProgressBar p1 = new ProgressBar();
+        p1.setMaxWidth(Double.MAX_VALUE);
+        p1.progressProperty().set(0);
+        ProgressBar p2 = new ProgressBar();
+        p2.setMaxWidth(Double.MAX_VALUE);
+        p2.progressProperty().set(0);
+        progressbar.getChildren().addAll(p1, p2);
+
+        Button start = new Button("Start");
+        start.setPadding(new Insets(10));
+
+        plus.setOnAction(event -> addProgressbar(progressbar));
+        minus.setOnAction(event -> subProgressbar());
+        start.setOnAction(event -> runThreads());
+
+        pane.setTop(label);
+        pane.setLeft(buttons);
+        pane.setCenter(progressbar);
+        pane.setRight(start);
+        pane.setBottom(textArea);
+
+        stage.show();
+    }
+
+    class Pferd extends Thread {
+        int id;
+        ProgressBar progressbar;
+        double progress = 0;
+
+        public Pferd(int id, Node progressbar) {
+            this.id = id;
+            this.progressbar = (ProgressBar) progressbar;
+        }
+
+        @Override
+        public void run() {
+            this.progress = 0.00;
+            while (this.progress < 1) {
+                double rnd = ((Math.random())*0.004 + 0.002);
+                this.progress += rnd;
+                System.out.println(rnd);
+                this.progressbar.progressProperty().set(progress);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            String text = textArea.getText();
+            textArea.setText(text + "\nDas Pferd " + this.id + " ist im Ziel!");
+        }
+    }
+
+    static class Konto {
+        int kontostand;
+
+        /**
+         * Empty constructor.
+         * initialises account with balance 0€
+         */
+        public Konto(){
+            kontostand = 0;
+        }
+
+        /**
+         * Constructor given all variables.
+         * @param kontostand balance that the account should be initialised with
+         */
+        public Konto(int kontostand){
+            this.kontostand = kontostand;
+        }
+
+        /**
+         * gets the current balance of an account
+         * @return current balance as an Integer
+         */
+        public int getKontostand() {
+            return this.kontostand;
+        }
+
+        /**
+         * sets the balance of an account
+         * @param kontostand balance to set to as an Integer
+         */
+        public void setKontostand(int kontostand) {
+            this.kontostand = kontostand;
+        }
+
+        /**
+         * adds a specific balance to an account
+         * @param betrag amount to add
+         */
+        void add(int betrag) {
+            int wert = getKontostand();
+            wert = wert + betrag;
+            setKontostand(wert);
+        }
+
+        /**
+         * Converts an account to a printable useful string.
+         * @return Kontostand + current balance with decimals and € as a String
+         */
+        @Override
+        public String toString(){
+            String kontostandstr = Integer.toString(kontostand);
+            StringBuilder output = new StringBuilder();
+            int helper = (kontostandstr.length()%3);
+            if (helper!=0) {
+                output.append(kontostandstr, 0, helper).append(".");
+            }
+            String tripples = kontostandstr.substring(helper);
+            for (int i = 0; i < tripples.length(); i+=3) {
+                output.append(tripples, i, i + 3).append(".");
+            }
+            return output.substring(0, output.length()-1) + "€";
+        }
+    }
+
 }
